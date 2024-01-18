443162beddfcac49bf82d47d6bd035f1389df6d9
Prapti Jaduvanshi <67221447+praptijaduvanshi@users.noreply.github.com>
2024-01-18 15:17:20 +0530

    Setting up global values and font

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ pygame.init()
 WIDTH, HEIGHT =  800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
+
+#Global values-- colors, font
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
+
+FONT = pygame.font.SysFont("comicsans", 16)
